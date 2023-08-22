2257326ea71eee54af0b8e11e87cf5a62cd2e484
mengq <54799613+meliniarindaa@users.noreply.github.com>
2023-08-22 23:05:27 +0700

    revisi keempat
    nambahin regist, login, dan view login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,14 +10,54 @@ class Auth extends CI_Controller
     }
     public function index()
     {
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('email','Email','trim|required|valid_email');
+        $this->form_validation->set_rules('password','Password','trim|required');
+        if($this->form_validation->run()==false){
+            $data['title']= 'Login Page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        }else{
+            //ketika validasi sukses
+            $this->_login();
+        }
+    }
+
+    private function _login(){
+        $email=$this->input->post('email');
+        $password=$this->input->post('password');
+
+        $user = $this->db->get_where('user',['email'=>$email])->row_array();
+        //jika user aktif
+        if ($user) {
+            if ($user['is_active']==1) {
+                # cek pw
+                if(password_verify($password,$user['password'])){
+                    $data=[
+                        'email'=> $user['email'],
+                        'role_id'=>$user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('user');
+                }else{
+                    $this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">
+                    Wrong password!</div>');
+                }
+            }else{
+                $this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">
+                This email has not been activated! </div>');
+            }
+        }else{
+            $this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">
+            Email is not registered!</div>');
+        }
     }
     public function registration()
     {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]',[
+            'is_unique'=>'This email has already registered!'
+        ]);
         $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[2]|matches[password2]', [
             'matches' => 'password dont match!',
             'min_length' => 'password too short!'
@@ -32,17 +72,28 @@ class Auth extends CI_Controller
             $this->load->view('templates/auth_footer');
         } else {
             $data = [
-                'name' => $this->input->post('name'),
-                'email' => $this->input->post('email'),
+                'name' => $this->input->post('name','true'),
+                'email' => $this->input->post('email','true'),
                 'image' => 'default.jpg',
-                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id' => 2,
                 'is_active' => 1,
                 'date_created' => time()
             ];
 
             $this->db->insert('user', $data);
+            $this->session->set_flashdata('message','<div class="alert alert-success" role="alert">
+            Congratulation! your account has been created. Please Login</div>');
             redirect('auth');
         }
+    }
+    public function logout(){
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        
+        $this->session->set_flashdata('message','<div class="alert alert-success" role="alert">
+        You have been logged out!</div>');
+        redirect('auth');
     }
 }
