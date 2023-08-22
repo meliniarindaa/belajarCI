4377d6a5e4d143d0614028a070617ef3c8ea7e8c
mengq <54799613+meliniarindaa@users.noreply.github.com>
2023-08-22 20:36:24 +0700

    revisi ketiga
    create

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -16,13 +16,33 @@ class Auth extends CI_Controller
     }
     public function registration()
     {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[2]|matches[password2]', [
+            'matches' => 'password dont match!',
+            'min_length' => 'password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+
         if ($this->form_validation->run() == false) {
             $data['title'] = 'User Registration Page';
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
         } else {
-            echo 'data berhasil ditambahkan!';
+            $data = [
+                'name' => $this->input->post('name'),
+                'email' => $this->input->post('email'),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            redirect('auth');
         }
     }
 }
